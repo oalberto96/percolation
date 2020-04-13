441831226f694997ceca11f65cc687c539b05981
oalberto96 <ochoapublic@gmail.com>
2020-04-13 08:52:19 -0700

    init percolation classes

---
+++ src/PercolationStats.java
@@ -0,0 +1,27 @@
+
+
+public class PercolationStats {
+    public PercolationStats(int n, int trials){
+
+    }
+
+    public double mean(){
+        return 0;
+    }
+
+    public double sttdev(){
+        return 0;
+    }
+
+    public double confidenceLo(){
+        return 0;
+    }
+
+    public double confidenceHi(){
+        return 0;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
